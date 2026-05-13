98a8d474bac1420465446601027d71c484db5a5c
Minagami-Yuiki <yuishiki.ryougi@gmail.com>
2026-05-13 10:33:18 +0800

    updata waymo

--- static/js/event_handler.js
+++ static/js/event_handler.js
@@ -37,12 +37,18 @@ document.addEventListener('DOMContentLoaded', domReady);
                 textPosition: "bottom"
 
             });
+            new Dics({
+                container: document.querySelectorAll('.b-dics')[6],
+                hideTexts: false,
+                textPosition: "bottom"
+
+            });
         }
 
         function replicargbdSceneEvent(idx) {
             let dics = document.querySelectorAll('.b-dics')[0]
             let sections = dics.getElementsByClassName('b-dics__section')
-            let imagesLength = 8
+            let imagesLength = 7
             for (let i = 0; i < imagesLength; i++) {
                 let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
                 switch (idx) {
@@ -316,15 +322,16 @@ document.addEventListener('DOMContentLoaded', domReady);
             }
         }
 
+
         function replicageomSceneEvent(idx) {
-            let dics = document.querySelectorAll('.b-dics')[5]
+            let dics = document.querySelectorAll('.b-dics')[6]
             let sections = dics.getElementsByClassName('b-dics__section')
-            let imagesLength = 8
+            let imagesLength = 6
             for (let i = 0; i < imagesLength; i++) {
                 let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
                 switch (idx) {
                     case 0:
-                        image.src = 'img/geom/replica/room0';
+                        image.src = 'img/geom/replica/room0/';
                         break;
                     case 1:
                         image.src = 'img/geom/replica/room1/';
@@ -372,6 +379,48 @@ document.addEventListener('DOMContentLoaded', domReady);
             }
 
             let scene_list = document.getElementById("replica-geom").children;
+            for (let i = 0; i < scene_list.length; i++) {
+                if (idx == i) {
+                    scene_list[i].children[0].className = "nav-link active"
+                }
+                else {
+                    scene_list[i].children[0].className = "nav-link"
+                }
+            }
+        }
+
+        
+        function waymoSceneEvent(idx) {
+            let dics = document.querySelectorAll('.b-dics')[5]
+            let sections = dics.getElementsByClassName('b-dics__section')
+            let imagesLength = 3
+            for (let i = 0; i < imagesLength; i++) {
+                let image = sections[i].getElementsByClassName('b-dics__image-container')[0].getElementsByClassName('b-dics__image')[0]
+                switch (idx) {
+                    case 0:
+                        image.src = 'img/waymo/106762/';
+                        break;
+                    case 1:
+                        image.src = 'img/waymo/158686/';
+                        break;
+                    case 2:
+                        image.src = 'img/waymo/405841/';
+                        break; 
+                }
+                switch (i) {
+                    case 0:
+                        image.src = image.src + 'opengsslam.png';
+                        break;
+                    case 1:
+                        image.src = image.src + 'ours.png';
+                        break;
+                    case 2:
+                        image.src = image.src + 'gt.png';
+                        break;
+                }
+            }
+
+            let scene_list = document.getElementById("waymo").children;
             for (let i = 0; i < scene_list.length; i++) {
                 if (idx == i) {
                     scene_list[i].children[0].className = "nav-link active"
